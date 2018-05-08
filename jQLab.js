0bf42cb8b33e1ba0a9ee2b77385ea07ecd6dd7c6
tylercschneider <tylercschneider@gmail.com>
2018-05-08 11:55:14 -0400

    bad commit msg

--- jQLab.js
+++ jQLab.js
@@ -1,4 +1,6 @@
 let id;
+let bookings = [];
+
 class Table {
 	draw(i) {
 		let content = $(`<h1></h1>`).text(i);
@@ -9,7 +11,6 @@ class Table {
 		$('#tableContainer').append(circle);
 
 		let table = $(`#${i}`);
-		console.log(table);
 		// opens form...
 		table.on("click", () => {
 			id = table[0].id;
@@ -19,14 +20,16 @@ class Table {
 			tableNumber.attr("id", "tableNumber");
 			let target = $('#target');
 			target.after(tableNumber);
+			$('#tableContainer').hide();
 			$('#form').show();
 		});
 	}
 
 }
 class Restaurant {
-	constructor(numOfTables) {
+	constructor(numOfTables, bookings) {
 		this.tables = numOfTables;
+		this.bookings = bookings;
 	}
 	layout() {
 		for(let i=1; i<(this.tables+1); i++){
@@ -44,9 +47,26 @@ $('#form').hide();
 // on empty table
 $('#close').on("click", () => {
 	$('#form').hide();
+	$('#tableContainer').show();
 	$('#tableNumber').remove();
 
 });
 $('#book').on("click", () => {
-	$(`#${id}`).
+	$(`#${id}`).removeClass('open').addClass('filled').prop('disabled', true);
+	let name = $('#name');
+	let phone = $('#phone');
+	let partySize = $('#party');
+	let booking = {
+		name: name.val(),
+		phone: phone.val(),
+		partySize: partySize.val()
+	};
+	bookings.push(booking);
+
+	$('#form').hide();
+	name.val("");
+	phone.val("");
+	partySize.val("");
+	$('#tableNumber').remove();
+	$('#tableContainer').show();
 });
